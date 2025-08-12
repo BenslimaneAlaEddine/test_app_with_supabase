7b74f4d69e97fcfd70238971b5c11d2d1896a27f
AlaEddine <alaeddineben181@gmail.com>
2025-08-12 16:21:17 +0100

    feat(auth): add logout feature using Supabase

--- lib/home.dart
+++ lib/home.dart
@@ -8,11 +8,35 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Welcome"),
-      ),
+      appBar: AppBar(title: const Text("Welcome"), actions: [
+        PopupMenuButton(onSelected: (value) async {
+          if (value == 0) {
+            final session = await Supabase.instance.client.auth.currentSession;
+            print(
+                "*****************************************************************************************");
+            print(session);
+            await Supabase.instance.client.auth.signOut();
+            final sessionEnd =
+                await Supabase.instance.client.auth.currentSession;
+            print(
+                "*****************************************************************************************");
+            print(sessionEnd);
+            print(response.user);
+          }
+        }, itemBuilder: (context) {
+          return [
+            const PopupMenuItem(
+              child: Text("Logout"),
+              value: 0,
+            )
+          ];
+        }),
+      ]),
       body: Center(
-        child: Text("Hi ${response.user?.email}"),
+        child: Text(
+          "Hi ${response.user?.email}: ",
+          overflow: TextOverflow.ellipsis,
+        ),
       ),
     );
   }
